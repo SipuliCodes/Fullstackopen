e299f3fb29e2c8b960b202eb1683618565dbd7d4
SipuliCodes <vepsalainensisu@gmail.com>
2023-10-18 17:13:59 +0300

    1.11*:unicafe_step6_valmis

--- osa 1/unicafe/src/App.jsx
+++ osa 1/unicafe/src/App.jsx
@@ -20,12 +20,34 @@ const Statistics = ({ good, neutral, bad, all, average, positive }) => {
   }
   return (
     <div>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive} isPercentage={true} />
+      <table>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td>{all}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{average}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td>{positive} %</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
