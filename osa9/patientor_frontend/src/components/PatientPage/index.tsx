dd156aa2d7f7c16749a4c0883d4d00f64814f5ab
SipuliCodes <vepsalainensisu@gmail.com>
2024-03-04 20:00:51 +0200

    osa9_teht24_valmis

--- osa9/patientor_frontend/src/components/PatientPage/index.tsx
+++ osa9/patientor_frontend/src/components/PatientPage/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { Patient } from "../../types";
+import { Diagnosis, Patient } from "../../types";
 import patientService from "../../services/patients";
+import diagnoseService from "../../services/diagnoses";
 import { useParams } from "react-router-dom";
 import FemaleIcon from '@mui/icons-material/Female';
 import MaleIcon from "@mui/icons-material/Male";
@@ -8,9 +9,12 @@ import MaleIcon from "@mui/icons-material/Male";
 const PatientPage = () => {
   const { id } = useParams < { id?: string }>();
   const [patient, setPatient] = useState<Patient>();
+  const [diagnoses, setDiagnoses] = useState<Diagnosis[]>();
 
   useEffect(() => {
-    
+    diagnoseService.getAll()
+      .then(diagnoses => setDiagnoses(diagnoses));
+
     if (id) {
       patientService.getPatient(id)
         .then(patient => {
@@ -28,7 +32,18 @@ const PatientPage = () => {
       <h2 style={{display: "inline"}}>{patient.name}</h2> {patient.gender === "female" ? <FemaleIcon /> : patient.gender === "male" ? <MaleIcon /> : null }
       <p>ssn: {patient.ssn}
       <br></br>
-      occupation: {patient.occupation}</p>
+        occupation: {patient.occupation}</p>
+      <h3>entries</h3>
+      {patient.entries.map(entry => {
+        return (
+          <div key={entry.id}>
+            {entry.date} {entry.description}
+            <ul>
+              {entry.diagnosisCodes && entry.diagnosisCodes.map(code => <li key={code}>{code} { diagnoses?.find(diagnose => diagnose.code === code)?.name }</li> )}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
